c56c24d7008b70ee2e1d3b18914f4de3935286ac
Artem <tipoya00@mail.ru>
2022-12-03 16:30:50 +0300

    prikoli

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const port = 80;
 app.use(express.static(__dirname));
 //app.use('/prikoli', express.static('./prikoli'));
 
-app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
+    app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
 
 app.post('/feedback',
     express.json(),
     async function (req, res) {
         let feedback = await database.setFeedbackToDB(req.body.picName, req.body.vote);
         res.send(feedback);
-            //.then(() => {res.send(database.getFeedbackOfDB(req.body.picName))});
 });
+
 
 app.get('/getPrikol', async (req, res) => {
     let badLink = await prikoli.getRandomPrikol();
